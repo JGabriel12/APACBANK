3782f36b6c8451a7e4980925f7e288dd2c4effe8
João Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2021-11-08 01:35:27 -0300

    UPDATES

---
+++ assets/php/conecta_db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "apacbank_database";
+
+$conexao = mysqli_connect($host, $user, $password, $database);
+mysqli_set_charset($conexao, 'utf8');
+
+
+
+// Conexão do banco de dados
+/* $mysqli = new mysqli($host, $user, $password, $database);
+
+if ($mysqli->connect_errno)
+  echo "Falha na conecção"; */
