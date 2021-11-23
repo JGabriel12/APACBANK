8c25aa8e90e01b568bda58b03fc71c002e743e51
João Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2021-11-22 21:08:38 -0300

    INSERT Depósito

--- assets/php/deposito.php
+++ assets/php/deposito.php
@@ -1,11 +1,23 @@
 <?php
 include 'conecta_db.php';
+$id_usuario = $_POST["id_usuario"];
 $saldo_conta = $_POST["saldo_conta"];
+$tipo_conta = $_POST["tipo_conta"];
 $deposito_atual = $_POST["deposito_atual"];
 
 if ($deposito_atual == null) {
   echo "Insira um saldo válido!";
 } else {
   $saldo_conta += $deposito_atual;
-  echo $saldo_conta;
+
+  $query = "INSERT INTO conta (saldo_conta, id_usuario, tipo_conta) VALUES ('$saldo_conta','$id_usuario','$tipo_conta')";
+
+  if ($conexao->query($query) === true) {
+    echo "<script>
+  alert('Depósito realizado com sucesso!');
+  window.location.href='../screens/listagem_usuarios.php';
+  </script>";
+  } else {
+    echo "Erro " . $query . "<br>" . $conexao->error;
+  }
 }
--- assets/screens/deposito.php
+++ assets/screens/deposito.php
@@ -26,6 +26,8 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
   <br>
   <form action="../php/deposito.php" method="POST">
     <input type="number" name="deposito_atual" id="deposito_atual" placeholder="Saldo para o depósito em R$">
+    <input type="hidden" name="id_usuario" id="id_usuario" value="<?php echo $id_usuario ?>">
+    <input type="hidden" name="tipo_conta" id="tipo_conta" value="<?php echo $tipo_conta ?>">
     <input type="hidden" name="saldo_conta" id="saldo_conta" value="<?php echo $saldo_conta ?>">
     <input type="submit" name="submit_deposito" id="submit_deposito" value="Depositar">
   </form>
