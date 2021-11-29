c3bf0b10d43767c576173349c3e4b031d6e8299b
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2021-11-29 09:14:05 -0300

    Readme and dump update

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # APACBANK
 
-O banco de investimentos da APAC
+<h1>O banco de investimentos da APAC </h1>
 
 Sistema para gerenciar os usuários que desejam abrir uma conta no banco ApacBank.
 
-Tecnologias usadas: PHP, HTML, CSS e MySQL.
+Funcionalidades do sistema: Cadastrar user e conta, deletar, listar, atualizar, depósito, saque e transferência.
+
+<h2>Regras:
+Só podem ser usadas as tecnologias: PHP, HTML, CSS e MySQL.
+Não pode usar framework. </h2>
+
+Cada usuário poderá, caso queira, abrir mais de um tipo de conta. Os tipos de
+conta disponíveis no banco são: Conta Corrente, Conta Poupança e Conta Jurídica. Cada conta
+do usuário deverá ter um número exclusivo que a identifique.
+
+Para conseguir abrir a conta, os usuários devem realizar um depósito inicial com os
+seguintes valores:
+
+    1. Conta corrente: R$ 100,00
+    2. Conta Poupança: R$ 500,00
+    3. Conta Jurídica: R$ 1000,00
+
+Os usuários podem fazer outros depósitos e aumentar os saldos das contas, assim como
+podem fazer saques e diminuir o saldo das contas. Cada depósito ou saque deve ficar
+armazenado para consultarmos o histórico futuramente. Gravar a data, o usuário envolvido e o
+valor depositado/sacado.
+
+Os usuários também podem fazer transferências de saldo com outros usuários do banco.
+Da mesma forma, cada transferência deve ficar armazenada para consultarmos o histórico
+futuramente. Gravar a data, os usuários envolvidos e o valor transferido.
+
+Um usuário pode querer encerrar sua conta no banco. Antes disso, ele precisará sacar
+todo o dinheiro do banco e em seguida ser inativado no sistema.
+
+Sistema feito somente para estudo :grinning:
--- assets/DUMP/apacbank.sql
+++ assets/DUMP/apacbank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Nov-2021 às 16:26
+-- Tempo de geração: 29-Nov-2021 às 13:04
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.0.11
 
@@ -41,15 +41,26 @@ CREATE TABLE `cadastro_usuario` (
 --
 
 INSERT INTO `cadastro_usuario` (`id_usuario`, `nome_usuario`, `cpf_usuario`, `senha_usuario`, `confirmaSenha_usuario`, `status_usuario`) VALUES
-(1, 'Joao Gabriel Marinho', '12923589475', '8833f1325fb6341757b30f6de91487', '8833f1325fb6341757b30f6de91487', 1),
+(1, 'Joao Gabriel Marinho De Andrade', '12923589475', '8833f1325fb6341757b30f6de91487', '8833f1325fb6341757b30f6de91487', 1),
 (2, 'Maria Nascimento', '13145387057', '263bce650e68ab4e23f28263760b9f', '263bce650e68ab4e23f28263760b9f', 1),
 (3, 'Thiago Henrique Carvalho', '40949670014', 'e99a18c428cb38d5f260853678922e', 'e99a18c428cb38d5f260853678922e', 1),
 (4, 'Maria de fatima', '31577214021', '81dc9bdb52d04dc20036dbd8313ed0', '81dc9bdb52d04dc20036dbd8313ed0', 1),
 (5, 'Rafael Texeira Sivirino', '06565068062', '827ccb0eea8a706c4c34a16891f84e', '827ccb0eea8a706c4c34a16891f84e', 1),
-(6, 'Maria Nascimento', '75994027057', '202cb962ac59075b964b07152d234b', '202cb962ac59075b964b07152d234b', 1),
-(7, 'Maria Nascimento', '91353084000', '81dc9bdb52d04dc20036dbd8313ed0', '81dc9bdb52d04dc20036dbd8313ed0', 1),
-(8, 'Joao Gabriel Marinho', '91353084000', '202cb962ac59075b964b07152d234b', '202cb962ac59075b964b07152d234b', 1),
-(9, 'Joao Gabriel Marinho', '91353084000', '202cb962ac59075b964b07152d234b', '202cb962ac59075b964b07152d234b', 1);
+(10, 'Mario Martins Santos', '96566610014', 'aeb34368c5d53aee32431b5386f71c', 'aeb34368c5d53aee32431b5386f71c', 1),
+(11, 'Fabio Texeira', '62893741096', '8cfa2282b17de0a598c010f5f0109e', '8cfa2282b17de0a598c010f5f0109e', 1),
+(12, 'Sivirna Nasimento', '42303295041', 'ed2b1f468c5f915f3f1cf75d7068ba', 'ed2b1f468c5f915f3f1cf75d7068ba', 1),
+(13, 'Roberta Silva', '28237593014', '928111ccbf11ffbb2bc5cf2681c656', '928111ccbf11ffbb2bc5cf2681c656', 1),
+(14, 'Sylvannas Correventos', '84410486063', 'd2f04c2a7cdcbe8efaef996a6249e7', 'd2f04c2a7cdcbe8efaef996a6249e7', 1),
+(15, 'Alexander Arnold', '98403756011', '4297f44b13955235245b2497399d7a', '4297f44b13955235245b2497399d7a', 0),
+(16, 'Samuel Marinho Dos Santos', '49692777022', '55493847dc2683bc0d35c7f9947e93', '55493847dc2683bc0d35c7f9947e93', 0),
+(17, 'Severino Santos', '54645738011', '4297f44b13955235245b2497399d7a', '4297f44b13955235245b2497399d7a', 1),
+(18, 'Guto Marinho', '91335711040', 'ed2b1f468c5f915f3f1cf75d7068ba', 'ed2b1f468c5f915f3f1cf75d7068ba', 1),
+(19, 'Silvia Lins', '95586627043', '9b2d75e8ef30064e6f9e8b006acae3', '9b2d75e8ef30064e6f9e8b006acae3', 1),
+(20, 'David Silva', '55878626071', '104e08045184a4f111170f990e274e', '104e08045184a4f111170f990e274e', 1),
+(21, 'Alex Sandro', '81411835085', '3aa24069c4d4418426a7c048ec5887', '3aa24069c4d4418426a7c048ec5887', 1),
+(22, 'Thiago Macedo', '23552324578', '19835d9787f9968f7d6e6650739f39', '19835d9787f9968f7d6e6650739f39', 0),
+(23, 'Marcos Silva', '95838493856', '93447ae568aedff4c3a4bf45ada41b', '93447ae568aedff4c3a4bf45ada41b', 1),
+(24, 'Junior Vieira', '29489338975', '9b2d75e8ef30064e6f9e8b006acae3', '9b2d75e8ef30064e6f9e8b006acae3', 1);
 
 -- --------------------------------------------------------
 
@@ -71,13 +82,26 @@ CREATE TABLE `conta` (
 --
 
 INSERT INTO `conta` (`id_conta`, `saldo_conta`, `data_de_criacao`, `tipo_conta`, `status_conta`, `id_usuario`) VALUES
-(4, 100, '2021-11-11', 'Conta corrente', 1, 1),
-(5, 500, '2021-11-11', 'Conta poupança', 1, 1),
-(9, 1000, '2021-11-11', 'Conta jurídica', 1, 1),
-(14, 100, '2021-11-11', 'Conta corrente', 1, 2),
-(15, 500, '2021-11-11', 'Conta poupança', 1, 2),
-(16, 100, '2021-11-11', 'Conta corrente', 1, 2),
-(17, 500, '2021-11-11', 'Conta poupança', 1, 2);
+(4, 1000, '2021-11-11', 'Conta corrente', 1, 1),
+(5, 100, '2021-11-11', 'Conta poupança', 0, 1),
+(9, 2100, '2021-11-11', 'Conta jurídica', 1, 1),
+(14, 510, '2021-11-11', 'Conta corrente', 1, 2),
+(15, 240, '2021-11-11', 'Conta poupança', 1, 2),
+(19, 1000, '2021-11-12', 'Conta jurídica', 1, 2),
+(27, 500, '2021-11-12', 'Conta poupança', 1, 15),
+(28, 100, '2021-11-16', 'Conta corrente', 1, 16),
+(45, 100, '2021-11-16', 'Conta corrente', 1, 14),
+(46, 100, '2021-11-16', 'Conta corrente', 1, 4),
+(47, 500, '2021-11-17', 'Conta poupança', 1, 18),
+(48, 100, '2021-11-17', 'Conta corrente', 1, 11),
+(49, 100, '2021-11-19', 'Conta corrente', 1, 19),
+(50, 1000, '2021-11-19', 'Conta jurídica', 1, 17),
+(51, 0, '2021-11-22', 'Conta corrente', 0, 20),
+(52, 0, '2021-11-23', 'Conta corrente', 0, 20),
+(53, 100, '2021-11-25', 'Conta corrente', 1, 3),
+(54, 100, '2021-11-25', 'Conta poupança', 1, 1),
+(55, 1000, '2021-11-25', 'Conta corrente', 0, 1),
+(56, 100, '2021-11-25', 'Conta corrente', 1, 21);
 
 -- --------------------------------------------------------
 
@@ -86,13 +110,60 @@ INSERT INTO `conta` (`id_conta`, `saldo_conta`, `data_de_criacao`, `tipo_conta`,
 --
 
 CREATE TABLE `transacao` (
-  `id_transacao` int(11) NOT NULL,
+  `id_conta` int(11) NOT NULL,
   `tipo_transacao` varchar(30) NOT NULL,
   `data_transacao` date NOT NULL,
   `valor_transacao` float NOT NULL,
   `id_conta_origem` int(11) NOT NULL,
   `id_conta_destino` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `transacao`
+--
+
+INSERT INTO `transacao` (`id_conta`, `tipo_transacao`, `data_transacao`, `valor_transacao`, `id_conta_origem`, `id_conta_destino`) VALUES
+(1, 'Transferência', '2021-11-24', 20, 4, 2),
+(2, 'Transferência', '2021-11-24', 200, 5, 2),
+(3, 'Transferência', '2021-11-24', 100, 5, 2),
+(4, 'Saque', '2021-11-24', 50, 5, 0),
+(5, 'Transferência', '2021-11-25', 50, 5, 2),
+(6, 'Depósito', '2021-11-25', 5000, 53, 0),
+(7, 'Saque', '2021-11-25', 5200, 53, 0),
+(8, 'Depósito', '2021-11-25', 200, 53, 0),
+(9, 'Saque', '2021-11-25', 700, 15, 0),
+(10, 'Depósito', '2021-11-25', 100, 15, 0),
+(11, 'Depósito', '2021-11-25', 50, 15, 0),
+(12, 'Depósito', '2021-11-25', 5, 15, 0),
+(13, 'Transferência', '2021-11-25', 110, 15, 2),
+(14, 'Depósito', '2021-11-25', 105, 15, 0),
+(15, 'Transferência', '2021-11-25', 50, 15, 2),
+(16, 'Transferência', '2021-11-25', 20, 15, 2),
+(17, 'Transferência', '2021-11-25', 20, 15, 2),
+(18, 'Transferência', '2021-11-25', 10, 15, 2),
+(19, 'Transferência', '2021-11-25', 10, 15, 2),
+(20, 'Transferência', '2021-11-25', 10, 15, 2),
+(21, 'Transferência', '2021-11-25', 10, 15, 2),
+(22, 'Transferência', '2021-11-25', 2700, 14, 1),
+(23, 'Depósito', '2021-11-25', 20, 14, 0),
+(24, 'Transferência', '2021-11-25', 10, 15, 2),
+(25, 'Transferência', '2021-11-25', 10, 14, 2),
+(26, 'Saque', '2021-11-25', 100, 55, 0),
+(27, 'Transferência', '2021-11-25', 100, 9, 1),
+(28, 'Depósito', '2021-11-25', 1000, 4, 0),
+(29, 'Transferência', '2021-11-25', 200, 4, 1),
+(30, 'Transferência', '2021-11-25', 200, 4, 1),
+(31, 'Transferência', '2021-11-25', 100, 4, 1),
+(32, 'Transferência', '2021-11-25', 100, 15, 2),
+(33, 'Transferência', '2021-11-25', 200, 4, 6),
+(34, 'Transferência', '2021-11-25', 100, 4, 1),
+(35, 'Transferência', '2021-11-25', 10, 14, 21),
+(36, 'Transferência', '2021-11-25', 100, 4, 2),
+(37, 'Transferência', '2021-11-25', 100, 4, 2),
+(38, 'Transferência', '2021-11-25', 100, 4, 2),
+(39, 'Transferência', '2021-11-25', 100, 4, 1),
+(40, 'Transferência', '2021-11-25', 200, 4, 2),
+(41, 'Transferência', '2021-11-25', 200, 4, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -115,9 +186,7 @@ ALTER TABLE `conta`
 -- Índices para tabela `transacao`
 --
 ALTER TABLE `transacao`
-  ADD PRIMARY KEY (`id_transacao`),
-  ADD KEY `id_conta_origem` (`id_conta_origem`),
-  ADD KEY `id_conta_destino` (`id_conta_destino`);
+  ADD PRIMARY KEY (`id_conta`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -127,19 +196,19 @@ ALTER TABLE `transacao`
 -- AUTO_INCREMENT de tabela `cadastro_usuario`
 --
 ALTER TABLE `cadastro_usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de tabela `conta`
 --
 ALTER TABLE `conta`
-  MODIFY `id_conta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_conta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT de tabela `transacao`
 --
 ALTER TABLE `transacao`
-  MODIFY `id_transacao` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_conta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- Restrições para despejos de tabelas
@@ -150,13 +219,6 @@ ALTER TABLE `transacao`
 --
 ALTER TABLE `conta`
   ADD CONSTRAINT `conta_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `cadastro_usuario` (`id_usuario`);
-
---
--- Limitadores para a tabela `transacao`
---
-ALTER TABLE `transacao`
-  ADD CONSTRAINT `transacao_ibfk_1` FOREIGN KEY (`id_conta_origem`) REFERENCES `conta` (`id_conta`),
-  ADD CONSTRAINT `transacao_ibfk_2` FOREIGN KEY (`id_conta_destino`) REFERENCES `conta` (`id_conta`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
